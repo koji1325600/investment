4029ea92e8d70deacd9166d7bb8fdba31df81b34
k-nagasawa <k-nagasawa>
2023-09-14 15:18:33 +0900

    add: ログイン機能追加

--- src/main/java/com/example/investment/controller/LoginController.java
+++ src/main/java/com/example/investment/controller/LoginController.java
@@ -1,5 +1,54 @@
 package com.example.investment.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.investment.dao.UserDao;
+import com.example.investment.repository.UserRepository;
+
+@Controller
 public class LoginController {
+	@Autowired
+    HttpServletRequest httpServletRequest;
+
+	@Autowired
+    UserRepository userRepository;
     
+    /** ログイン画面遷移 */
+	@GetMapping(path="login")
+	String login(Model model) {
+		return "/Login";
+	}
+
+	/** ログイン処理を行う */
+    @PostMapping(path="investLogin")
+    String todoLogin(@RequestParam String mailaddress, String password, Pbkdf2PasswordEncoder passwordEncoder, Model model){
+        UserDao userDao = userRepository.findByMailaddressDao(mailaddress);
+        if (userDao == null) {
+            //ログイン画面に戻る
+            return "redirect:/login";
+        }
+        //パスワードがDBと一致しなかった場合
+        if (!passwordEncoder.matches(password,userDao.getPassword())) {
+            //ログイン画面に戻る
+            return "redirect:/login";
+        }
+        //Sessionにユーザ名を設定
+        httpServletRequest.getSession().setAttribute("userId", userDao.getUserId());
+        return "redirect:invest/investList";
+    }
+
+    /** ログアウト */
+	@GetMapping(path = "logout")
+	String logout(HttpServletRequest httpServletRequest){
+		httpServletRequest.getSession().invalidate();
+		return "/Login";
+	}
 }

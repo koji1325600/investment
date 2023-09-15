7a9391585f592ba78910c13d2e7e22ce97a41198
k-nagasawa <k-nagasawa>
2023-09-15 15:45:19 +0900

    add: 購入処理追加・修正

--- src/main/java/com/example/investment/service/InvestmentService.java
+++ src/main/java/com/example/investment/service/InvestmentService.java
@@ -9,9 +9,13 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.investment.dao.BuyingDao;
 import com.example.investment.dao.InvestmentDao;
+import com.example.investment.dao.UserDao;
 import com.example.investment.form.InvestmentForm;
+import com.example.investment.repository.BuyingRepository;
 import com.example.investment.repository.InvestmentRepository;
+import com.example.investment.repository.UserRepository;
 
 @Service
 public class InvestmentService {
@@ -20,6 +24,12 @@ public class InvestmentService {
 
     @Autowired
     InvestmentRepository investmentRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BuyingRepository buyingRepository;
 
     /** 取引追加処理 */
     public void addInvent(InvestmentForm investmentForm) {
@@ -57,6 +67,11 @@ public class InvestmentService {
         if (price < investDao.getCrash()) {
             System.out.println("暴落");
             price = rand.nextInt(rangePrice) + (minPrice + minRangePrice);
+            List<BuyingDao> buyingList = buyingRepository.findByInvestIdList(investDao.getId());
+
+            for (BuyingDao buyingDao: buyingList) {
+                buyingRepository.delete(buyingDao);
+            }
         }
 
         // 価格が最大価格より上にならないように
@@ -66,5 +81,36 @@ public class InvestmentService {
         System.out.println("set:" + price);
         investDao.setPrice(price);
         return investDao;
+    }
+
+    /** 購入処理 */
+    public void buying(String id, int quantity) {
+        InvestmentDao investDao = investmentRepository.findById(id).get();
+        String userId = httpServletRequest.getSession().getAttribute("userId").toString();
+        UserDao userDao = userRepository.findById(userId).get();
+        BuyingDao buyingDao = buyingRepository.findByInvestIdAndUserIdDao(id, userId);
+
+        if (buyingDao == null) {
+            buyingDao = new BuyingDao();
+            buyingDao.addTodoDao();
+            buyingDao.setInvestId(id);
+            buyingDao.setInvestName(investDao.getName());
+            buyingDao.setUserId(userId);
+            buyingDao.setUserName(userDao.getUserName());
+            buyingDao.setQuantity(quantity);
+            buyingDao.setBuyPrice(investDao.getPrice());
+        } else {
+            quantity = quantity + buyingDao.getQuantity();
+            buyingDao.setQuantity(quantity);
+            buyingDao.setBuyPrice(investDao.getPrice());
+        }
+
+        buyingRepository.save(buyingDao);
+
+        int totalPrice = investDao.getPrice() * quantity;
+        int money = userDao.getMoney();
+
+        userDao.setMoney(money - totalPrice);
+        userRepository.save(userDao);
     }
 }
